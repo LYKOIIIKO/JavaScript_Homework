fb6394f78fd9b4e83ee270ef06fc8294ea67cdcb
Anton Haba <lykoiiiko@gmail.com>
2024-11-09 18:25:53 +0300

    working on homework 5 11/13

--- hw5/script.js
+++ hw5/script.js
@@ -157,8 +157,6 @@ console.log('-----------Задание 7-----------');
 // кроме пробела, то пирамида должна быть нарисована этим символом,
 // например:
 
-// Курс «JS».
-
 // *
 // **
 // ***
@@ -168,3 +166,178 @@ console.log('-----------Задание 7-----------');
 // *******
 // ********
 // *********
+
+function pyramid(pyramidHeigth) {
+	for(let i = 1; i <= pyramidHeigth; i++) {
+		let line = '';
+
+		for (let j = 1; j <= (i * 2) - 1; j++) {
+			if(arguments.length > 1 && arguments[1] != ' ') line +=arguments[1]; //проверка на наличие доп параметра
+			else line += i;
+		}
+		console.log(line);
+	}
+}
+
+let pyramidHeigth = +prompt('Введите высоту пирамиды');
+pyramid(pyramidHeigth, "*");
+
+console.log('-----------Задание 8-----------');
+// 8. Напишите ф-цию, которая рисует равнобедренный треугольник из
+// звездочек:
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// Кол-во рядов должно вводиться с клавиатуры. Доп., напишите такую же ф-
+// цию, но которая выведет перевернутый треугольник.
+
+function triangle(triangleHeigth) {
+	for(let i = 1; i <= triangleHeigth; i++) {
+		let line = '';
+
+		for (let j = 1; j <= triangleHeigth - i; j++) {
+			line += ' ';
+		}
+	
+		for (let j = 1; j <= (i * 2) - 1; j++) {
+			line += '*';
+		}
+		console.log(line);
+	}
+}
+
+function triangleReverse(triangleHeigth) {
+	for(let i = 1; i <= triangleHeigth; i++) {
+		let line = '';
+
+		for (let j = 1; j <= (i - 1); j++) {
+			line += ' ';
+		}
+	
+		for (let j = 1; j <= ((triangleHeigth * 2) - ((i * 2) - 1)); j++) {
+			line += '*';
+		}
+		console.log(line);
+	}
+}
+
+let triangleHeigth = +prompt('Введите высоту равнобедренного треугольника');
+triangle(triangleHeigth);
+triangleReverse(triangleHeigth);
+
+console.log('-----------Задание 9-----------');
+//9. Напишите ф-цию, которая возвращает массив заполненный числами Фибоначи от 0 до 1000.
+
+function fibonacci() {
+	let arr = [0, 1];
+	for(let i = 2; (arr[i - 1] + arr[i - 2]) <= 1000; i++) {
+		arr.push(arr[i - 1] + arr[i - 2]);
+	}
+
+	return arr;
+}
+
+console.log(fibonacci());
+
+console.log('-----------Задание 10-----------');
+//10. Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его цифры. И так, пока сумма не станет однозначным числом (9 и менее). Исп. Рекурсию.
+// Я НЕ ПОНИМАЮ УСЛОВИЕ
+function numCheck(num) {
+	let sum = null;
+	for (let i = 0; i < num.length; i++) {
+		sum += +num[i];
+	}
+	return sum;
+}
+
+console.log(numCheck('333'));
+
+console.log('-----------Задание 11-----------');
+//11. Дан массив с числами (передается параметром). Выведите последовательно его элементы, используя рекурсию и не используя цикл.
+
+function printArray(arr) {
+	console.log(arr[i]);
+	i++;
+	if (i < arr.length) printArray(arr);
+}
+
+let i = 0;
+printArray([0,1,2,3,4,5]);
+
+console.log('-----------Задание 12-----------');
+// //12. Напишите ф-цию, запрашивающую имя, фамилия, отчество и номер
+// группы студента и выводящую введённые данные в следующем виде:
+
+// *****************************
+// * Домашняя работа: «Функции» *
+// * Выполнил: студент гр. W4017 *
+// * Иванов Иван Иванович *
+// *****************************
+
+// Размер рамки должен определятся автоматически по самой длинной строке.
+// Рамку вывести в консоль.
+//ЭТО БЫЛО ЖЕСТКО
+
+function showHomework() {
+	let firstName = prompt('Введите имя'),
+		lastName = prompt('Введите фамилию'),
+		middleName = prompt('Введите отчество'),
+		group = prompt('Введите номер группы'),
+		string1 = '',
+		string2 = `* Домашняя работа: «Функции» `,
+		string3 = `* Выполнил: студент гр. ${group} `,
+		string4 = `* ${lastName} ${firstName} ${middleName} `,
+		length = null;
+
+	if(string2.length > string3.length && string2.length > string4.length) {
+		length = string2.length + 1;
+	} else if (string3.length > string2.length && string3.length > string4.length) {
+		length = string3.length + 1;
+	} else if (string4.length > string2.length && string4.length > string3.length) {
+		length = string4.length + 1;
+	}
+
+	for (let i = 0; i < length; i++) {
+		string1 += '*';
+	}
+
+	if(string2.length < length) {
+		stringPush = '';
+		for(let i = 1; i < (length - string2.length); i++) {
+			stringPush += ' ';
+		}
+		string2 += stringPush + '*';
+	}
+
+	if(string3.length < length) {
+		stringPush = '';
+		for(let i = 1; i < (length - string3.length); i++) {
+			stringPush += ' ';
+		}
+		string3 += stringPush + '*';
+	}
+
+	if(string4.length < length) {
+		stringPush = '';
+		for(let i = 1; i < (length - string4.length); i++) {
+			stringPush += ' ';
+		}
+		string4 += stringPush + '*';
+	}
+
+	console.log(string1);
+	console.log(string2);
+	console.log(string3);
+	console.log(string4);
+	console.log(string1);
+}
+
+showHomework();
+
+console.log('-----------Задание 13-----------');
+//13. Напишите ф-цию, которая должна проверить правильность ввода адреса эл. почты, неиспользуя регулярные выражения. Почта верна при условии: a. весь адрес не должен содержать русские буквы и спецсимволы, кроме одной «собачки», знака подчеркивания, дефиса и точки, причем они не могут быть первыми и последними в адресе, и идти подряд, например: «..», «@.», «.@» или «@@», «_@», «@-», «--» и т.п. b. имя эл. почты (до знака @) должно быть длиной более 2 символов, причем имя может содержать только буквы, цифры, но не быть первыми и единственными в имени, и точку; c. после последней точки и после @, домен верхнего уровня (ru, by, com и т.п.) не может быть длиной менее 2 и более 11 символов.
+
