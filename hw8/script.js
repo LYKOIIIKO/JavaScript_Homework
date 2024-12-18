979a39f603e5b3dddaadf20eaeea991a70dab08d
Anton Haba <lykoiiiko@gmail.com>
2024-12-18 11:12:41 +0300

    complete hw8 100%

--- hw8/script.js
+++ hw8/script.js
@@ -84,6 +84,131 @@ function site () {
 	site.append(container);
 
 	document.body.append(site);
+
+	//добавим стиля
+	let style = document.createElement('style');
+	style.innerHTML = `
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+
+		ul, ol {
+			list-style: none;
+		}
+
+		.container {
+			max-width: 1280px;
+			width: 90%;
+			margin: 50px auto;
+		}
+
+		.main__title,
+		.promo__title {
+			text-align: center;
+			font-family: "Arvo", serif;
+			font-size: 36px;
+			line-height: 48px;
+			color: #212121;
+			font-weight: normal;
+		}
+
+		.main__title {
+			margin-bottom: 15px;
+		}
+
+		.main__text {
+			display: flex;
+			justify-content: center;
+
+			font-family: "Open Sans", serif;
+			font-size: 14px;
+			line-height: 26px;
+			color: #9FA3A7;
+
+			margin-bottom: 50px;
+
+		}
+
+		.promo {
+			display: flex;
+			justify-content: center;
+		}
+
+		.promo__block {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 15px;
+
+			width: 35%;
+			padding: 80px 95px;
+		}
+
+		.promo__block:hover {
+			background-color: #8F75BE;
+		}
+
+		.promo__block:hover .promo__title,
+		.promo__block:hover .promo__text,
+		.promo__block:hover .btn-primary {
+			color: #fff;
+			background-color: #8F75BE;
+		}
+
+		.promo__block:hover .promo__subtitle {
+			color: #FFC80A;
+		}
+
+		.promo__subtitle {
+			font-family: "Montserrat", sans-serif;
+			font-size: 12px;
+			font-weight: bold;
+			letter-spacing: 2.4px;
+			color: #9FA3A7;
+			text-transform: uppercase;
+		}
+
+		.promo__title {
+			margin-bottom: 10px;
+		}
+
+		.promo__text {
+			font-family: "Open Sans", serif;
+			font-size: 12px;
+			line-height: 22px;
+			color: #9FA3A7;
+
+			text-align: center;
+
+			margin-bottom: 50px;
+		}
+
+		.btn-primary {
+			border: 3px solid #FFC80A;
+			border-radius: 30px;
+			padding: 15px 24px;
+
+			font-family: "Montserrat", sans-serif;
+			font-size: 12px;
+			font-weight: bold;
+			letter-spacing: 2.4px;
+			color: #212121;
+			text-transform: uppercase;
+
+			cursor: pointer;
+		}
+	`;
+
+	head.appendChild(style);
+
+	//подключим шрифты
+	let fonts = document.createElement('link');
+	fonts.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+	fonts.setAttribute('rel', 'stylesheet');
+
+	head.insertBefore(fonts, document.querySelector('script'));
 }
 
 
