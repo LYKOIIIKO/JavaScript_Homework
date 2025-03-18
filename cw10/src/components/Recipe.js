c089f1356fa4ecbb25d2d3d5e3c4542c295f0f65
Anton Haba <lykoiiiko@gmail.com>
2025-03-18 14:51:58 +0300

    cw10 complete

--- cw10/src/components/Recipe.js
+++ cw10/src/components/Recipe.js
@@ -1,34 +1,13 @@
-import { tag } from './Tag.js';
-class Page {
+class Recipe {
 	constructor() {
-		this.id = null;
-		this.title = 'Tags';
-	}
-
-	getId() {
-		let href = location.href;
-		href = href.split('/');
-
-		if (!href || href.length == 0) return;
-
-		return href[href.length - 1];
+		this.title = null;
 	}
 
 	create() {
 		let elem = document.createElement('div');
-		elem.classList.add('main__content','content');
+		elem.classList.add('recipes__content');
 
-			let elemRecipes = document.createElement('div');
-			elemRecipes.classList.add('recipes__content');
-
-		return [elem, elemRecipes];
-	}
-
-	checkTag(tag, id) {
-		tag.childNodes.forEach(item =>{
-			item.classList.remove('active');
-			if (item.innerText == `${id} >`) item.classList.add('active');
-		})
+		return elem;
 	}
 
 	createRecipes(data) {
@@ -84,31 +63,19 @@ class Page {
 		.then(response => response.json())
 		.then(data => {
 			data.recipes.forEach(item => {
-				this.elemRecipes.append(this.createRecipes(item));
+				this.elem.append(this.createRecipes(item));
 			});
 		})
 	}
 
 	init() {
-		this.id = this.getId();
+		this.elem = this.create();
 
-		this.elems = this.create();
-		this.elem = this.elems[0];
-		this.elemRecipes = this.elems[1];
-
-		
-		
-		this.checkTag(tag, this.id);
-		
-		if (this.id) this.getData(`https://dummyjson.com/recipes/tag/${this.id}`);
-		
-		if (tag) this.elem.append(tag, this.elemRecipes);
+		this.getData('https://dummyjson.com/recipes');
 
 		return this.elem;
 	}
 }
 
-let obj = new Page();
-let elem = obj.init();
-let elemTitle = obj.id;
-export {elem as page, elemTitle as pageTitle};
+let recipe = new Recipe().init();
+export {recipe};
