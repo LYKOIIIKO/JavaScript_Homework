d61da7a1fe5fd28013e2aa5ff4da83050038a15c
Anton Haba <lykoiiiko@gmail.com>
2025-04-08 15:25:10 +0300

    JS lesson. Webpack. Use in hw14 SPA site

---
+++ react1/scripts/index.js
@@ -0,0 +1,11 @@
+import Header from "./compionents/Layout/Header.js"
+import Footer from "./compionents/Layout/Footer.js"
+import Main from "./compionents/Layout/Main.js"
+
+const root = document.getElementById('root')
+
+const header = new Header().render()
+const footer = new Footer().render()
+const main = new Main().render()
+
+root.append(header, main, footer)
