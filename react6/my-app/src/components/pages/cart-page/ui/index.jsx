574005350f3a6510c018d6fbc2f7c32a589ee4fa
Anton Haba <lykoiiiko@gmail.com>
2025-04-30 11:56:24 +0300

    fast fix

--- react6/my-app/src/components/pages/cart-page/ui/index.jsx
+++ react6/my-app/src/components/pages/cart-page/ui/index.jsx
@@ -2,19 +2,21 @@ import { useCart } from "../../../shared/context/CartContext";
 import CartItem from "./components/CartItem/CartItem";
 
 const CartPage = () => {
-	const { cart, clear } = useCart()
+	const { cart, clear } = useCart();
 
 	return (
 		<div>
 			<h1>Products</h1>
-			<div>
-				{cart.map(item => <CartItem key={item.id.toString()} product={item} />)}
-			</div>
-			{cart.length && (
-				<button onClick={clear}>Clear all</button>
+			{!!cart.length && (
+				<div>
+					{cart.map((item) => (
+						<CartItem key={item.id.toString()} product={item} />
+					))}
+				</div>
 			)}
+			{!!cart.length && <button onClick={clear}>Clear all</button>}
 		</div>
-	)
+	);
 };
 
-export default CartPage
+export default CartPage;
